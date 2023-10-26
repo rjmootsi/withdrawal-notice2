27968ac612b20d0e2971095ea4b9f25bdb7abd74
John Mootsi <jmootsi021@student.wethinkcode.co.za>
2023-10-26 18:16:15 +0200

    Initial project

---
+++ src/main/java/com/enviro/assessment/grad001/johnmootsi/withdrawalnotice/WithdrawalNoticeApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.johnmootsi.withdrawalnotice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithdrawalNoticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithdrawalNoticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enviro/assessment/grad001/johnmootsi/withdrawalnotice/WithdrawalNoticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.johnmootsi.withdrawalnotice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithdrawalNoticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
